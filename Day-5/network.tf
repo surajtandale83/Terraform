86dc5f6b3599f743a203cb400b28ef470042cc18
Surajtandale <suraj.tandale@gmail.com>
2025-02-26 15:02:16 +0530

    add state file

--- Day-5/network.tf
+++ Day-5/network.tf
@@ -1,17 +1,55 @@
-#Crerate New VPC with Terraform
-resource "aws_vpc" "VPC_with_terraform" 
-{
-    cidr_blocks = "10.0.0.0/16"
-    tags = {
-        Name ="my-vpc_terraform"
+#Create a new VPC with natgatway and private
+resource "aws_vpc" "MyVPC" {
+    cidr_block = "10.0.0.0/16"
+    tags ={
+    Name = "MyVPC"
     }
-  
 }
-#Create subnet in VPC with Terraform
-resource ="subnet" "sunbent_with_terraform"{
-    vpc_id = aws_vpc.VPC_with_terraform.id
-    cidr_blocks = "10.0.0.0/16"
-    tags
+
+resource "aws_subnet" "mypublicsubnet" { #Publ;ic subnet1
+    vpc_id = aws_vpc.MyVPC.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "ap-southeast-2a"
+    tags = {
+      Name = "mypublicsubnet"
+          }
+
+}
+resource "aws_subnet" "mypublicsubbnet2" { #Publ;ic subnet2
+    vpc_id = aws_vpc.MyVPC.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "ap-southeast-2b"
+    tags = {
+      Name = "mypublicsubnet2"
     }
+}   
+resource "aws_subnet" "myprivatesubnet" { # Private Subnet1
+    vpc_id = aws_vpc.MyVPC.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "ap-southeast-2c"
+      tags = {
+    Name = "MyPrivateSubnets"
+    }
+  }
+
+  resource "aws_subnet" "name" { # Private Subnet2
+    vpc_id =aws_vpc.MyVPC.id
+    cidr_block = "10.0.4.0/24"
+    availability_zone = "ap-southeast-2d"
+    tags ={
+    Name = "MyPrivateSubnets2"
+    }
+}
+  
+ resource "aws_route_table" "MyPublicRouteTable" {
+    vpc_id = aws_vpc.MyVPC.id
+    tags = {
+        Name = "MyPublicRouteTable"     
+    }
+ }
+ resource "aws" "name" {
    
- 
+ }
+
+
+  
--- Day3-Staatefile/main.tf
+++ Day3-Staatefile/main.tf
@@ -1,6 +1,20 @@
-resource "aws_instance" "terraform_instance" {
+resource "aws_instance" "terraform_instance1" {
     ami = "ami-0c50b6f7dc3701ddd"
     instance_type = "t2.micro"
     key_name = "multicloud"
+    tags = {
+        Name = "Prod"
+    }
   
 }
+
+resource "aws_instance" "terraform_instance2" {
+    ami = "ami-0c50b6f7dc3701ddd"
+    instance_type = "t2.nano"
+    key_name = "multicloud"
+    tags = {
+        Name = "Test"
+    }
+  
+}
+
